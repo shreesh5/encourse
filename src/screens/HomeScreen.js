d1c32510c83d9eb4a1b145de65f3d7dffb84c2f4
Shreesh Nayak <shreeshnayak@gmail.com>
2021-05-18 01:46:06 -0700

    Setup navigation

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {HomeScreenStyles as styles} from '../styles/Home';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.contentView}>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
---
+++ src/styles/Home.js
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+
+export const HomeScreenStyles = StyleSheet.create({
+  contentView: {
+    flex: 1,
+    padding: 5,
+  },
+});
