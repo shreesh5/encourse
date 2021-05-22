b18252ad0d154e3f47cc078e5d628f96f147e0c4
Shreesh Nayak <shreeshnayak@gmail.com>
2021-05-22 16:19:27 -0700

    Added react native testing library and initial test for button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 
-const Button = ({label, onPress, labelStyle, containerStyle}) => {
+const Button = ({label, onPress, labelStyle, containerStyle, testID}) => {
   return (
-    <TouchableOpacity onPress={() => onPress()}>
+    <TouchableOpacity onPress={() => onPress()} testID={testID}>
       <View style={{...containerStyle}}>
         <Text style={{...labelStyle}}>{label}</Text>
       </View>
