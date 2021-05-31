bc44a463227083792321ed5c7c25194db1ae2cb3
Shreesh Nayak <shreeshnayak@gmail.com>
2021-05-31 13:10:23 -0700

    Updated resolve auth screen

--- src/screens/ResolveAuthScreen.js
+++ src/screens/ResolveAuthScreen.js
@@ -1,4 +1,5 @@
-import React, {useEffect, useContext} from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, {useEffect} from 'react';
 import {useAuthContext} from '../context/AuthContext';
 
 const ResolveAuthScreen = () => {
@@ -6,6 +7,7 @@ const ResolveAuthScreen = () => {
 
   useEffect(() => {
     tryLocalSignin();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return null;
