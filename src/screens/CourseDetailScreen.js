d541cc7d21485a71fbec9a26e986f2ab6adfe4d5
Shreesh Nayak <shreeshnayak@gmail.com>
2021-05-31 11:41:52 -0700

    Updated course detail screen

--- src/screens/CourseDetailScreen.js
+++ src/screens/CourseDetailScreen.js
@@ -1,6 +1,5 @@
-import React, {useContext, useState, useEffect} from 'react';
-import {View, Text, ActivityIndicator} from 'react-native';
-import {Context as AuthContext} from '../context/AuthContext';
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
 import {useCourseContext} from '../context/CourseContext';
 import {useAuthContext} from '../context/AuthContext';
 import {CourseStyles as styles} from '../styles/Course';
@@ -9,16 +8,8 @@ import Button from '../components/Button';
 
 const CourseDetailScreen = ({navigation}) => {
   const [course, setCourse] = useState(navigation.getParam('course'));
-  const [loading, setLoading] = useState(false);
-  const {state: courseState, deleteCourse} = useCourseContext();
+  const {deleteCourse} = useCourseContext();
   const {state: authState} = useAuthContext();
-
-  // useEffect(() => {
-  //   const id = navigation.getParam('id');
-  //   const courseInfo = courseState.courses.find((c) => c.id === id);
-  //   setCourse(courseInfo);
-  //   setLoading(false);
-  // },[]);
 
   const enrollInCourse = async (userId, courseId) => {
     try {
@@ -46,77 +37,69 @@ const CourseDetailScreen = ({navigation}) => {
     }
   };
 
-  const courseDetail = () => {
-    return (
-      <>
-        <Text style={styles.courseName}>{course.name}</Text>
-        <View style={styles.courseDurationContainer}>
-          <Text style={styles.label}>Duration:</Text>
-          <Text style={styles.text}>{course.duration}</Text>
-        </View>
-        <View style={styles.courseDescriptionContainer}>
-          <Text style={styles.label}>Description:</Text>
-          <Text style={styles.text}>{course.description}</Text>
-        </View>
-        <View style={styles.courseDurationContainer}>
-          <Text style={styles.label}>Capacity:</Text>
-          <Text style={styles.text}>
-            {course.capacity - course.users.length} / {course.capacity}
-          </Text>
-        </View>
-        <View style={styles.allButtonContainer}>
-          <View style={styles.buttonContainer}>
-            {!course.users.includes(parseInt(authState.pk)) ? (
-              <Button
-                label="Add"
-                onPress={() => enrollInCourse(authState.pk, course.id)}
-                labelStyle={styles.buttonText}
-                containerStyle={styles.enrollButton}
-                testID="enroll-button"
-              />
-            ) : (
-              <Button
-                label="Drop"
-                onPress={() => unEnrollInCourse(authState.pk, course.id)}
-                labelStyle={styles.buttonText}
-                containerStyle={styles.enrollButton}
-                testID="drop-button"
-              />
-            )}
-          </View>
-          {authState.role === 'superuser' ? (
-            <View style={styles.buttonContainer}>
-              <Button
-                label="Edit Course"
-                onPress={() =>
-                  navigation.navigate('EditCourse', {
-                    course,
-                    onGoBack: (updatedCourse) => {
-                      setCourse(updatedCourse);
-                    },
-                  })
-                }
-                labelStyle={styles.buttonText}
-                containerStyle={styles.adminButton}
-                testID="edit-button"
-              />
-              <Button
-                label="Delete Course"
-                onPress={() => deleteCourse(course.id)}
-                labelStyle={styles.buttonText}
-                containerStyle={styles.adminButton}
-                testID="delete-button"
-              />
-            </View>
-          ) : null}
-        </View>
-      </>
-    );
-  };
-
   return (
     <View style={styles.contentView}>
-      {loading ? <ActivityIndicator /> : courseDetail()}
+      <Text style={styles.courseName}>{course.name}</Text>
+      <View style={styles.courseDurationContainer}>
+        <Text style={styles.label}>Duration:</Text>
+        <Text style={styles.text}>{course.duration}</Text>
+      </View>
+      <View style={styles.courseDescriptionContainer}>
+        <Text style={styles.label}>Description:</Text>
+        <Text style={styles.text}>{course.description}</Text>
+      </View>
+      <View style={styles.courseDurationContainer}>
+        <Text style={styles.label}>Capacity:</Text>
+        <Text style={styles.text}>
+          {course.capacity - course.users.length} / {course.capacity}
+        </Text>
+      </View>
+      <View style={styles.allButtonContainer}>
+        <View style={styles.buttonContainer}>
+          {!course.users.includes(parseInt(authState.pk, 10)) ? (
+            <Button
+              label="Add"
+              onPress={() => enrollInCourse(authState.pk, course.id)}
+              labelStyle={styles.buttonText}
+              containerStyle={styles.enrollButton}
+              testID="enroll-button"
+            />
+          ) : (
+            <Button
+              label="Drop"
+              onPress={() => unEnrollInCourse(authState.pk, course.id)}
+              labelStyle={styles.buttonText}
+              containerStyle={styles.enrollButton}
+              testID="drop-button"
+            />
+          )}
+        </View>
+        {authState.role === 'superuser' ? (
+          <View style={styles.buttonContainer}>
+            <Button
+              label="Edit Course"
+              onPress={() =>
+                navigation.navigate('EditCourse', {
+                  course,
+                  onGoBack: (updatedCourse) => {
+                    setCourse(updatedCourse);
+                  },
+                })
+              }
+              labelStyle={styles.buttonText}
+              containerStyle={styles.adminButton}
+              testID="edit-button"
+            />
+            <Button
+              label="Delete Course"
+              onPress={() => deleteCourse(course.id)}
+              labelStyle={styles.buttonText}
+              containerStyle={styles.adminButton}
+              testID="delete-button"
+            />
+          </View>
+        ) : null}
+      </View>
     </View>
   );
 };
