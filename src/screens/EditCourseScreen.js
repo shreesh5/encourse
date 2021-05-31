6dd8c0c0e91d4d736c8b63d21008843ccdd8f55f
Shreesh Nayak <shreeshnayak@gmail.com>
2021-05-31 13:05:22 -0700

    Updated edit course screen

--- src/screens/EditCourseScreen.js
+++ src/screens/EditCourseScreen.js
@@ -1,57 +1,44 @@
-import React, {useContext, useState, useEffect} from 'react';
-import {View, ActivityIndicator} from 'react-native';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 import {useCourseContext} from '../context/CourseContext';
-import {CourseDetailStyles as styles} from '../styles/Course';
+import {CourseStyles as styles} from '../styles/Course';
 import CourseForm from '../components/CourseForm';
 
 const EditCourseScreen = ({navigation}) => {
+  // eslint-disable-next-line no-unused-vars
   const [course, setCourse] = useState(navigation.getParam('course'));
-  const [loading, setLoading] = useState(false);
   const {state: courseState, updateCourse} = useCourseContext();
-
-  // useEffect(() => {
-  //   const id = navigation.getParam('id');
-  //   const courseInfo = courseState.courses.find((c) => c.id === id);
-  //   setCourse(courseInfo);
-  //   setLoading(false);
-  // },[]);
 
   return (
     <View style={styles.contentView}>
-      {loading ? (
-        <ActivityIndicator />
-      ) : (
-        <CourseForm
-          buttonTestID="updatecourse-button"
-          headerText="Edit Course"
-          errorMessage={courseState.errorMessage}
-          submitButtonText="Save"
-          initialValues={{
-            id: course.id,
-            name: course.name,
-            duration: course.duration,
-            description: course.description,
-            capacity: course.capacity.toString(),
-            users: course.users,
-          }}
-          onSubmit={(id, name, duration, description, capacity, users) => {
-            updateCourse(id, name, duration, description, capacity, () => {
-              setLoading(true);
-              const updatedCourse = {
-                id,
-                name,
-                duration,
-                description,
-                capacity,
-                users,
-              };
-              navigation.getParam('onGoBack')(updatedCourse);
-              setLoading(false);
-              navigation.pop();
-            });
-          }}
-        />
-      )}
+      <CourseForm
+        buttonTestID="updatecourse-button"
+        headerText="Edit Course"
+        errorMessage={courseState.errorMessage}
+        submitButtonText="Save"
+        initialValues={{
+          id: course.id,
+          name: course.name,
+          duration: course.duration,
+          description: course.description,
+          capacity: course.capacity.toString(),
+          users: course.users,
+        }}
+        onSubmit={(id, name, duration, description, capacity, users) => {
+          updateCourse(id, name, duration, description, capacity, () => {
+            const updatedCourse = {
+              id,
+              name,
+              duration,
+              description,
+              capacity,
+              users,
+            };
+            navigation.getParam('onGoBack')(updatedCourse);
+            navigation.pop();
+          });
+        }}
+      />
     </View>
   );
 };
